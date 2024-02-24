45fe85096232d5fdfc04109ac8145e6e20dc6807
Eunjae <110014960+eunjoyourlife@users.noreply.github.com>
2024-02-24 17:32:06 +0900

    Update 3week.cpp

--- 3week.cpp
+++ 3week.cpp
@@ -1,59 +1,52 @@
 #include <iostream>
-#include <cassert>
-#include <fstream>
 
 using namespace std;
 
-bool CheckSorted(int* arr, int size)
-{
-	int sum = 0;
-	for (int i = 0; i < size-1; i++)
-	{
-		if (arr[i] > arr[i + 1])
-			return false;
-	}
+class student {
+private:
+	char * name;
+	int age;
+	char * vision;
+public:
+	void ShowInfo();
+	void SetInfo(char * _name, int _age, char * _vision);
+	void Study();
+	void Sleep();
+};
 
-	return true;
-	
+void student::ShowInfo()
+{
+	cout << "이름: " << name << ", 나이: " << age << ", 장래희망: " << vision << endl;
 }
 
-void Print(int* arr, int size)
+void student::SetInfo(char * _name, int _age, char * _vision)
 {
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+	name = _name;
+	age = _age;
+	vision = _vision;
+}
+
+void student::Study()
+{
+	cout << "공부!" << endl;
+}
+
+void student::Sleep()
+{
+	cout << "잠!" << endl;
 }
 
 int main()
 {
-	// Selection Sort
-	// 힌트: swap()
-	
-		// 8 3 2 5 
-		// 2 3 8 5
-		// 2 3 8 5
-		// 2 3 5 8 
-		// 2 3 5 8 
+	student stu;
 
-		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 };
-		int size = sizeof(arr) / sizeof(arr[0]);
+	stu.SetInfo("조은재", 21, "소프트웨어개발자");
+	stu.ShowInfo();
 
-		int min_index ;
-		for (int i = 0; i < size-1 ; i++)
-		{
-			min_index = i;
-			for (int j = i + 1; j < size; j++)
-			{
-				if (arr[min_index] > arr[j])
-					min_index = j;
-			}
-			swap(arr[i], arr[min_index]);
-			
-			Print(arr, size);
+	while(true) {
+		stu.Study();
+		stu.Sleep();
+	}
 
-			cout << boolalpha;
-			cout << CheckSorted(arr, size);
-			cout << endl;
-		}
-
+	return 0;
 }
